abda0a241b7d37df7941c5a15b36a13c46b39bcd
pengyanmingjack@gmail.com <Pym123456>
2019-05-13 11:49:21 +0800

    添加刷新状态

--- app/src/main/java/com/commonadapter/BaseViewModel.java
+++ app/src/main/java/com/commonadapter/BaseViewModel.java
@@ -25,6 +25,7 @@ public class BaseViewModel extends BaseObservable implements IBaseViewModel {
     public BaseViewModel() {
     }
 
+
     public BaseViewModel(Context context) {
         this.context = context;
         initDialog();
